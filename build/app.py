db7f9de7faacb72df415ea4ec478b2d94bf1d585
Ulbricht, Marian <ulbricht@innoroute.de>
2021-06-30 21:55:28 +0200

    addcall

--- build/app.py
+++ build/app.py
@@ -15,8 +15,28 @@ def get():
 	print(path)
 	try:
 		client = Client(client)
+		client.connect()
 		root = client.get_root_node()
 		resp=str(root.get_child(path).get_value())
+		client.disconnect()
+		return jsonify({'status':'OK','msg':"","response":str(resp)}),200
+	except Exception as e:
+		return jsonify({'status':'ERROR','msg':str(e)}),401
+
+@app.route('/call',  methods=['POST'])
+def call():
+	request_data=request.get_json(force=True)
+	client=request_data["CLIENT"]
+	path=request_data["PATH"]
+	method=request_data["METHOD"]
+	print(client)
+	print(path)
+	try:
+		client = Client(client)
+		client.connect()
+		root = client.get_root_node()
+		
+		resp=str(root.get_child(path).call_method(method,request_data["PARAM1"] ))
 		client.disconnect()
 		return jsonify({'status':'OK','msg':"","response":str(resp)}),200
 	except Exception as e:
@@ -25,3 +45,8 @@ def get():
 	
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5001)
+    
+    
+    
+    
+    
--- tests/test_get.sh
+++ tests/test_get.sh
@@ -1,2 +1,2 @@
-curl localhost:5001/get -d '{"CLIENT": "opc.tcp://localhost:4840/piccolo/node/","PATH":["0:Objects", "0:PiccoloNode", "0:resources_available", "0:memory"]}' -H 'Content-Type: application/json'
+curl localhost:5001/get -d '{"CLIENT": "opc.tcp://192.168.1.110:4840/piccolo/node/","PATH":["0:Objects", "0:PiccoloNode", "0:motor", "0:speed"]}' -H 'Content-Type: application/json'
 
