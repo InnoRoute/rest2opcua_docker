4e8d13ca5ca3200da400a997e6b824676939691c
Ulbricht, Marian <ulbricht@innoroute.de>
2022-03-26 19:32:33 +0100

    test

--- tests/test_call.sh
+++ tests/test_call.sh
@@ -1,2 +1,2 @@
-curl localhost:5001/call -d '{"CLIENT": "opc.tcp://192.168.1.110:4840/piccolo/node/","PATH":["0:Objects", "0:PiccoloNode",  "0:motor"],"METHOD":"0:set_speed","PARAM1":10}' -H 'Content-Type: application/json'
+curl localhost:9000/call -d '{"CLIENT": "opc.tcp://192.168.1.110:4840/piccolo/node/","PATH":["0:Objects", "0:PiccoloNode",  "0:motor"],"METHOD":"0:set_speed","PARAM1":20}' -H 'Content-Type: application/json'
 
